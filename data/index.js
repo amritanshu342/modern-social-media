35e3f2010f73deb70fb863f3abf5b1c27726ca5c
amritanshu342 <mcusuperclip@gmail.com>
2023-01-17 17:46:18 +0530

    first commit

---
+++ data/index.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userIds = [
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+];
+
+export const users = [
+  
+];
+
+
+
+
+
+
+
+
+
+
+
+
+export const posts = [
+  
+];
